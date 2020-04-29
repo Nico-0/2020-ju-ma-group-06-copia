8d1d46ab2b602c600b7e9ef10685b81504acbc08
Nico <nitraceros@gmail.com>
2020-04-29 00:59:38 -0300

    Clase DummyDesign con metodo integrante1

---
+++ src/main/java/dominio/DummyDesign.java
@@ -0,0 +1,15 @@
+package dominio;
+
+public class DummyDesign {
+	//1 Androchuk Nicolas
+	//2 Barreiro Walter
+	//3 Brea Juan
+	//4 Nisimura Juan
+	//5 Sanchez Venturini Ezequiel
+	
+	public int integrante1(){
+		
+		return 1;
+	}
+
+}
