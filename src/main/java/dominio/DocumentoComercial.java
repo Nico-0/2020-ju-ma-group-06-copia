f198b64a87599bae60da4368162af1e21ff84c82
juan-nisimura <juanisimura@gmail.com>
2020-05-12 00:09:36 -0300

    Extraigo enum TipoDocumentoComercial de DocumentoComercial

--- src/main/java/dominio/DocumentoComercial.java
+++ src/main/java/dominio/DocumentoComercial.java
@@ -3,8 +3,5 @@ package dominio;
 public class DocumentoComercial {
 
 	private int numero_documento;
-	public enum Tipo{
-		FACTURA, TICKET, SIN_DOCUMENTO                    //es probable que despues sean clases
-	}
-	
+	private TipoDocumentoComercial tipoDocumentoComercial;
 }
---
+++ src/main/java/dominio/TipoDocumentoComercial.java
@@ -0,0 +1,5 @@
+package dominio;
+
+public enum TipoDocumentoComercial{
+	FACTURA, TICKET, SIN_DOCUMENTO                    //es probable que despues sean clases
+}
