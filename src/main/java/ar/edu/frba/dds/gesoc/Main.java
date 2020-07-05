d252612e7f8379213a6898f8ff761abf635f46e9
Nico <nitraceros@gmail.com>
2020-07-04 21:31:01 -0300

    ejecutable

---
+++ src/main/java/ar/edu/frba/dds/gesoc/Main.java
@@ -0,0 +1,11 @@
+package ar.edu.frba.dds.gesoc;
+
+public class Main {
+	
+public static void main (String[] args) {
+	System.out.println("hola soy el programa ejecutable");
+	//se crea el ejecutable en la carpeta target luego de "mvn clean install"
+	//ejecutar con "java -jar PruebaConcepto-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
+}
+
+}
