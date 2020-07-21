9be7832f9ff6788a4e84d69ab9955e24cc43ff08
unknown <barreirowalter@live.com.ar>
2020-07-21 15:49:58 -0300

    Agrego reporte y etiquetas

--- src/main/java/dominio/compra/Compra.java
+++ src/main/java/dominio/compra/Compra.java
@@ -1,8 +1,11 @@
 package dominio.compra;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.TextStyle;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import org.apache.commons.lang3.Validate;
 
@@ -19,23 +22,38 @@ public class Compra {
 	private DocumentoComercial documentoComercial; // puede ser nulo
 	private LocalDate fecha;
 	private List<Usuario> usuariosRevisores = new ArrayList<>();
+	private String etiqueta;
 	// private Entidad entidad;
 
 	public double valor_total() {
 		return detalle.getTotal();
 	}
 
+	public String mes() {
+		Month mes = fecha.getMonth();
+		String nombre = mes.getDisplayName(TextStyle.FULL, new Locale("es", "ES"));
+		
+		String primeraLetra = nombre.substring(0,1);
+		String mayuscula = primeraLetra.toUpperCase();
+		String demasLetras = nombre.substring(1, nombre.length());
+		nombre = mayuscula + demasLetras;
+
+		return nombre;
+	}
+	
 	public Compra(Proveedor proveedor, 
 				  MedioPago medioPago, 
 				  LocalDate fecha,
 				  List<Presupuesto> presupuestos,
 				  Detalle detalle,
-				  List<Usuario> usuariosRevisores) {
+				  List<Usuario> usuariosRevisores,
+				  String etiqueta) {
 		Validate.notNull(proveedor, "proveedor faltante");
 		Validate.notNull(medioPago, "medio de pago faltante");
 		Validate.notNull(fecha, "fecha faltante");
 		Validate.notNull(detalle, "detalle faltante");
 		Validate.notNull(usuariosRevisores, "usuarios faltante");
+		Validate.notNull(etiqueta, "etiqueta faltante");
 		// Preconditions.validateNotNull(entidad,"entidad faltante");
 		this.proveedor = proveedor;
 		this.medioPago = medioPago;
@@ -43,11 +61,20 @@ public class Compra {
 		this.detalle = detalle;
 		this.usuariosRevisores = usuariosRevisores;
 		this.presupuestos = presupuestos;
+		this.etiqueta = etiqueta;
 		// this.entidad = entidad;
 	}
 
 	public void setDocumentoComercial(DocumentoComercial documentoComercial) {
 		this.documentoComercial = documentoComercial;
+	}
+	
+	public boolean esDelMes(String mes) {
+		return this.mes().equals(mes);
+	}
+	
+	public String etiqueta() {
+		return etiqueta;
 	}
 
 }
--- src/main/java/dominio/entidad/Entidad.java
+++ src/main/java/dominio/entidad/Entidad.java
@@ -1,7 +1,13 @@
 package dominio.entidad;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.TextStyle;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Set;
 
 import dominio.compra.Compra;
 
@@ -9,10 +15,16 @@ public abstract class Entidad {
 	public List<Compra> compras = new ArrayList<Compra>();
 	protected String nombreFicticio;
 	private List<EntidadBase> entidades_usadas = new ArrayList<EntidadBase>();
+	private LocalDate fecha;
+	private Reporte reporte;
 	
 	public void agregarCompra(Compra unaCompra) {
 		compras.add(unaCompra);
 	}
 	
-
+	public HashMap<String, List<Compra>> generarReporte(){
+		reporte = new Reporte();
+		return reporte.generarReporte(compras);
+	}
+	
 }
