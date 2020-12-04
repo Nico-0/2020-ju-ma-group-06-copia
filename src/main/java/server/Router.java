e876367855ce3cb8fecda3e02bf8f902cfa0b827
juan-nisimura <juanisimura@gmail.com>
2020-12-04 02:45:55 -0300

    Cambio ruta

--- src/main/java/server/Router.java
+++ src/main/java/server/Router.java
@@ -175,6 +175,6 @@ public class Router {
             PerThreadEntityManagers.closeEntityManager();
 		});
 		
+		
 	}
-
 }
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -10,9 +10,18 @@ import spark.debug.DebugScreen;
 public class Server {
 	public static void main(String[] args) throws FileNotFoundException, NoSuchAlgorithmException, InvalidKeySpecException {
 		//Spark.port(9000);
+		Spark.port(getHerokuAssignedPort());
 		DebugScreen.enableDebugScreen();
 		Bootstrap.init();
 		Router.configure();
 	}
 
+	static int getHerokuAssignedPort() {
+	    ProcessBuilder processBuilder = new ProcessBuilder();
+	    if (processBuilder.environment().get("PORT") != null) {
+	        return Integer.parseInt(processBuilder.environment().get("PORT"));
+	    }
+	    
+	    return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+	}
 }
