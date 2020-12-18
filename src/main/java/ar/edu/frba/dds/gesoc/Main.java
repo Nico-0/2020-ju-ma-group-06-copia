61de51f0663e5451c495901164be7616d4e9d015
Nico <nitraceros@gmail.com>
2020-12-18 08:35:03 -0300

    jobs heroku

--- src/main/java/ar/edu/frba/dds/gesoc/Main.java
+++ src/main/java/ar/edu/frba/dds/gesoc/Main.java
@@ -1,9 +1,17 @@
 package ar.edu.frba.dds.gesoc;
 
 import java.util.TimerTask;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import org.uqbarproject.jpa.java8.extras.PerThreadEntityManagers;
+
+import java.util.List;
 import java.util.Timer;
 import java.lang.System;
 import dominio.TareaProgramada;
+import dominio.presupuestos.CompraPendiente;
 
 public class Main {
 	
@@ -12,7 +20,15 @@ public class Main {
 public static void main (String[] args) {
 	
 	System.out.println("hola soy el programa ejecutable");
-	TareaProgramada tareaProgramada = new TareaProgramada(2000);
+	//TareaProgramada tareaProgramada = new TareaProgramada(2000);
+	
+	
+    final EntityManager entityManager = PerThreadEntityManagers.getEntityManager();
+	final EntityTransaction transaction = entityManager.getTransaction();
+	List<CompraPendiente> comprasPendientes = entityManager.createQuery("from CompraPendiente").getResultList();    	
+	transaction.begin();
+	comprasPendientes.stream().forEach(compraPendiente -> compraPendiente.validarCompra());
+	transaction.commit();
 
 	}
 
